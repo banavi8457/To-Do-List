05a6c16c1cecaa61cd10e8ea0f755d3e09f44e39
banavi8457 <102586467+banavi8457@users.noreply.github.com>
2022-05-22 11:05:25 +0530

    Add files via upload

---
+++ build/precache-manifest.30e245abd04b7a1d67b90a06a1f1d788.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be38b8c6c2d7071df44",
+    "url": "/static/css/main.8483ecf6.chunk.css"
+  },
+  {
+    "revision": "2be38b8c6c2d7071df44",
+    "url": "/static/js/main.2be38b8c.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "b92f3378c0ff8339e665",
+    "url": "/static/js/2.b92f3378.chunk.js"
+  },
+  {
+    "revision": "d9ce6d317115bc41b4f8",
+    "url": "/static/js/runtime~main.d9ce6d31.js"
+  },
+  {
+    "revision": "fac153b214046f609f2a29950e4d4aa1",
+    "url": "/index.html"
+  }
+];
